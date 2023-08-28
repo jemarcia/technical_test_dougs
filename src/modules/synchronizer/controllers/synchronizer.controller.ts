687c3cde160c2198e8740cdbfcdb58f623f25445
jeremy <jemarcia@student.42.fr>
2023-08-29 01:39:31 +0200

    Code with Exception

--- src/modules/synchronizer/controllers/synchronizer.controller.ts
+++ src/modules/synchronizer/controllers/synchronizer.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Logger, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+  Logger,
+  Post
+} from '@nestjs/common';
 import { SynchronizerService } from '../services/synchronizer.service';
 import { BodyDataDto } from '../dtos/body.dto';
 
@@ -8,11 +16,25 @@ export class SynchronizerController {
 
   private readonly logger = new Logger('SynchronizerController');
 
+  @HttpCode(HttpStatus.OK)
   @Post('/movements/validation')
   async validateMovements(
     @Body()
     { movements, balances }: BodyDataDto
   ) {
-    return this.synchronizerService.checkMovements(movements, balances);
+    const result = this.synchronizerService.checkMovements(movements, balances);
+
+    if (result === 'Accepted') {
+      return { statusCode: HttpStatus.ACCEPTED, message: 'Accepted' };
+    } else {
+      throw new HttpException(
+        {
+          status: HttpStatus.UNPROCESSABLE_ENTITY,
+          error: 'Unprocessable Entity',
+          reasons: result
+        },
+        HttpStatus.UNPROCESSABLE_ENTITY
+      );
+    }
   }
 }
--- src/modules/synchronizer/services/synchronizer.service.ts
+++ src/modules/synchronizer/services/synchronizer.service.ts
@@ -9,20 +9,15 @@ export class SynchronizerService {
     balances.sort((a, b) => a.date.getTime() - b.date.getTime());
 
     const firstDateBalances = balances[0].date;
-    const operationsBeforeFirstBalance = movements.filter(
+    const movementsBeforeFirstBalance = movements.filter(
       (mv) => mv.date.getTime() < firstDateBalances.getTime()
     );
 
-    if (operationsBeforeFirstBalance.length > 0) {
+    if (movementsBeforeFirstBalance.length > 0) {
       return {
-        message: 'I’m a teapot',
-        reasons: [
-          {
-            message:
-              'Certaines opérations ne peuvent pas être vérifiées car leur date est antérieure au plus ancien point de contrôle, veuillez les retirer pour procéder à la vérification.',
-            invalidOperations: operationsBeforeFirstBalance
-          }
-        ]
+        reason:
+          'Certaines opérations ne peuvent pas être vérifiées car leur date est antérieure au plus ancien point de contrôle, veuillez les retirer pour procéder à la vérification.',
+        invalidMovements: movementsBeforeFirstBalance
       };
     }
 
@@ -47,23 +42,20 @@ export class SynchronizerService {
 
       if (currentBalance !== balances[i + 1].balance) {
         const imbalanceReason = {
+          reason: 'Déséquilibre entre mouvements et balance',
           balanceDate: balances[i + 1].date,
           expectedBalance: balances[i + 1].balance,
           actualBalance: currentBalance,
-          reasons: [
-            {
-              reason: 'Déséquilibre entre mouvements et balance',
-              invalidMovements: periodMovements
-            }
-          ]
+          invalidMovements: periodMovements,
+          potentialCauses: []
         };
 
         // Chercher des mouvements strictement égaux
         const strictlyEqualMovements =
           this.findStrictlyEqualMovements(periodMovements);
         if (strictlyEqualMovements.length) {
-          imbalanceReason.reasons.push({
-            reason: 'Ces opérations sont strictement égales',
+          imbalanceReason.potentialCauses.push({
+            potentialCause: 'Ces opérations sont strictement égales',
             invalidMovements: strictlyEqualMovements
           });
         }
@@ -74,8 +66,8 @@ export class SynchronizerService {
           'id'
         );
         if (sameIDMovements.length) {
-          imbalanceReason.reasons.push({
-            reason: 'Ces opérations ont le même identifiant',
+          imbalanceReason.potentialCauses.push({
+            potentialCause: 'Ces opérations ont le même identifiant',
             invalidMovements: sameIDMovements
           });
         }
@@ -85,8 +77,8 @@ export class SynchronizerService {
           'date'
         );
         if (sameDates.length) {
-          imbalanceReason.reasons.push({
-            reason: 'Ces opérations ont la même date',
+          imbalanceReason.potentialCauses.push({
+            potentialCause: 'Ces opérations ont la même date',
             invalidMovements: sameDates
           });
         }
@@ -96,27 +88,20 @@ export class SynchronizerService {
           'amount'
         );
         if (sameAmounts.length) {
-          imbalanceReason.reasons.push({
-            reason: 'Ces opérations ont le même montant',
+          imbalanceReason.potentialCauses.push({
+            potentialCause: 'Ces opérations ont le même montant',
             invalidMovements: sameAmounts
           });
         }
-
-        // Ajoutez d'autres appels de fonction ici pour d'autres critères si nécessaire.
 
         reasons.push(imbalanceReason);
       }
     }
 
     if (reasons.length === 0) {
-      return {
-        message: 'Accepted'
-      };
+      return 'Accepted';
     } else {
-      return {
-        message: 'I’m a teapot',
-        reasons: reasons
-      };
+      return reasons;
     }
   }
 
@@ -172,183 +157,4 @@ export class SynchronizerService {
     }
     return results;
   }
-
-  // checkMovements(movements: Array<MovementDto>, balances: Array<BalanceDto>) {
-  //   movements.sort((a, b) => a.date.getTime() - b.date.getTime());
-  //   balances.sort((a, b) => a.date.getTime() - b.date.getTime());
-
-  //   const firstDateBalances = balances[0].date;
-  //   const operationsBeforeFirstBalance = movements.filter(
-  //     (mv) => mv.date.getTime() < firstDateBalances.getTime()
-  //   );
-
-  //   if (operationsBeforeFirstBalance.length > 0) {
-  //     return {
-  //       message: 'I’m a teapot',
-  //       reasons: [
-  //         {
-  //           message:
-  //             'Certaines opérations ne peuvent pas être vérifiées car leur date est antérieure au plus ancien point de contrôle',
-  //           invalidOperations: operationsBeforeFirstBalance
-  //         }
-  //       ]
-  //     };
-  //   }
-  //   const reasons = [];
-  //   let lastCheckedMovementIndex = 0;
-
-  //   for (let i = 0; i < balances.length - 1; i++) {
-  //     let currentBalance = balances[i].balance;
-  //     const periodMovements = [];
-
-  //     for (let j = lastCheckedMovementIndex; j < movements.length; j++) {
-  //       const movement = movements[j];
-
-  //       if (
-  //         new Date(movement.date).getTime() <
-  //         new Date(balances[i + 1].date).getTime()
-  //       ) {
-  //         currentBalance += movement.amount;
-  //         periodMovements.push(movement);
-  //         lastCheckedMovementIndex = j + 1;
-  //       } else {
-  //         break;
-  //       }
-  //     }
-
-  //     if (currentBalance !== balances[i + 1].balance) {
-  //       const invalidReasons = [
-  //         {
-  //           reason: 'Déséquilibre entre mouvements et balance',
-  //           invalidMovements: periodMovements
-  //         }
-  //       ];
-
-  //       const strictlyEquals = this.findStrictlyEqualMovements(periodMovements);
-  //       if (strictlyEquals.length > 0) {
-  //         invalidReasons.push({
-  //           reason: 'Ces opérations sont strictement égales',
-  //           invalidMovements: strictlyEquals.flat()
-  //         });
-  //       }
-
-  //       const filteredMovements = this.filterOutStrictlyEqualMovements(
-  //         periodMovements,
-  //         strictlyEquals
-  //       );
-  //       const sameIds = this.findMovementsWithSameProperty(
-  //         filteredMovements,
-  //         'id'
-  //       );
-  //       const sameDates = this.findMovementsWithSameProperty(
-  //         filteredMovements,
-  //         'date'
-  //       );
-  //       const sameAmounts = this.findMovementsWithSameProperty(
-  //         filteredMovements,
-  //         'amount'
-  //       );
-
-  //       if (sameIds.length > 0) {
-  //         invalidReasons.push({
-  //           reason: 'Ces opérations ont le même identifiant',
-  //           invalidMovements: sameIds.flat()
-  //         });
-  //       }
-  //       if (sameDates.length > 0) {
-  //         invalidReasons.push({
-  //           reason: 'Ces opérations ont la même date',
-  //           invalidMovements: sameDates.flat()
-  //         });
-  //       }
-  //       if (sameAmounts.length > 0) {
-  //         invalidReasons.push({
-  //           reason: 'Ces opérations ont le même montant',
-  //           invalidMovements: sameAmounts.flat()
-  //         });
-  //       }
-
-  //       reasons.push({
-  //         balanceDate: balances[i + 1].date,
-  //         expectedBalance: balances[i + 1].balance,
-  //         actualBalance: currentBalance,
-  //         reasons: invalidReasons
-  //       });
-  //     }
-  //   }
-
-  //   if (reasons.length === 0) {
-  //     return {
-  //       message: 'Accepted'
-  //     };
-  //   } else {
-  //     return {
-  //       message: 'I’m a teapot',
-  //       reasons: reasons
-  //     };
-  //   }
-  // }
-
-  // findMovementsWithSameProperty(
-  //   movements: Array<MovementDto>,
-  //   property: keyof MovementDto
-  // ): Array<Array<MovementDto>> {
-  //   const seen = new Map();
-  //   const duplicates = [];
-
-  //   for (const movement of movements) {
-  //     const value = movement[property];
-  //     if (seen.has(value)) {
-  //       seen.get(value).push(movement);
-  //     } else {
-  //       seen.set(value, [movement]);
-  //     }
-  //   }
-
-  //   // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  //   for (const [_, group] of seen.entries()) {
-  //     if (group.length > 1) {
-  //       duplicates.push(group);
-  //     }
-  //   }
-
-  //   return duplicates;
-  // }
-
-  // filterOutStrictlyEqualMovements(
-  //   movements: Array<MovementDto>,
-  //   strictlyEquals: Array<Array<MovementDto>>
-  // ): Array<MovementDto> {
-  //   const strictlyEqualSet = new Set(
-  //     strictlyEquals.flat().map((mov) => mov.id)
-  //   );
-  //   return movements.filter((mov) => !strictlyEqualSet.has(mov.id));
-  // }
-
-  // findStrictlyEqualMovements(
-  //   movements: Array<MovementDto>
-  // ): Array<Array<MovementDto>> {
-  //   const duplicates = [];
-  //   const seen = new Set<string>();
-
-  //   for (let i = 0; i < movements.length; i++) {
-  //     const currentMovement = movements[i];
-  //     const currentMovementStr = JSON.stringify(currentMovement);
-  //     if (seen.has(currentMovementStr)) {
-  //       continue; // Ignore if we've already seen this exact movement
-  //     }
-
-  //     const equalMovements = movements.filter((movement, index) => {
-  //       if (index <= i) return false; // Ignore previous and current movements
-  //       return JSON.stringify(movement) === currentMovementStr;
-  //     });
-
-  //     if (equalMovements.length > 0) {
-  //       duplicates.push([currentMovement, ...equalMovements]);
-  //       seen.add(currentMovementStr);
-  //     }
-  //   }
-
-  //   return duplicates;
-  // }
 }
