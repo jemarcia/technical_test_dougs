80a50664f58ef36618cd1281d5af266bd713cbd2
jeremy <jemarcia@student.42.fr>
2023-08-28 13:29:23 +0200

    Check date

--- src/modules/synchronizer/controllers/synchronizer.controller.ts
+++ src/modules/synchronizer/controllers/synchronizer.controller.ts
@@ -1,13 +1,6 @@
 import { Controller } from '@nestjs/common';
-import { MovementDto } from '../dtos/movement.dto';
-import { BalanceDto } from '../dtos/balance.dto';
 
 @Controller()
 export class SynchronizerController {
   constructor() {}
-
-  checkOperations(movements: Array<MovementDto>, balances: Array<BalanceDto>) {
-    movements.sort((a, b) => a.date.getTime() - b.date.getTime());
-    balances.sort((a, b) => a.date.getTime() - b.date.getTime());
-  }
 }
--- src/modules/synchronizer/services/synchronizer.service.ts
+++ src/modules/synchronizer/services/synchronizer.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { MovementDto } from '../dtos/movement.dto';
+import { BalanceDto } from '../dtos/balance.dto';
 
 @Injectable()
-export class SynchronizerService {}
+export class SynchronizerService {
+  checkOperations(movements: Array<MovementDto>, balances: Array<BalanceDto>) {
+    movements.sort((a, b) => a.date.getTime() - b.date.getTime());
+    balances.sort((a, b) => a.date.getTime() - b.date.getTime());
+
+    const firstDateBalances = balances[0].date;
+    const operationsBeforeFirstBalance = movements.filter(
+      (mv) => mv.date.getTime() < firstDateBalances.getTime()
+    );
+
+    if (operationsBeforeFirstBalance.length > 0) {
+      anomalies.push({
+        message:
+          'Certaines opérations ne peuvent pas être vérifiées car leur date est antérieure au plus ancien point de contrôle',
+        invalidOperations: operationsBeforeFirstBalance
+      });
+      return { message: 'I’m a teapot', reasons: anomalies };
+    }
+  }
+}
